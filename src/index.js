7d81f0f031fdbc849c016e30be7f980fba42c5b5
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-07-12 15:43:22 +0900

    #2.5 useNetwork

--- src/index.js
+++ src/index.js
@@ -7,33 +7,37 @@ import React, {
 } from "react";
 import ReactDOM from "react-dom";
 
-const useFadeIn = (duration = 1, delay = 0) => {
-  const element = useRef();
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
   useEffect(() => {
-    if (
-      typeof duration !== "number" ||
-      typeof delay !== "number"
-    ) {
-      console.log("return");
-      return;
-    }
-    if (element.current) {
-      console.log(element.current.style);
-      const { current } = element;
-      current.style.opacity = 1;
-      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
-    }
+    console.log("useEffect");
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    return () => {
+      console.log("componentwillunmount");
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
   }, []);
-  return { ref: element, style: { opacity: 0 } };
+  return status;
 };
 
 const App = () => {
-  const fadeInH1 = useFadeIn(2, 2);
-  const fadeInP = useFadeIn(4, 4);
+  const handleNetworkChange = (online) => {
+    console.log(
+      online ? "We just went online" : "We are offline"
+    );
+  };
+  const onLine = useNetwork(handleNetworkChange);
   return (
     <div className="App">
-      <h1 {...fadeInH1}>Hello</h1>
-      <p {...fadeInP}>alsdkfj;lsadj fl;kasjd lf;</p>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </div>
   );
 };
